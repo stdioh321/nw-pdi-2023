a1b85e812f821db12eb347498a215b3cccbedf1f
Henrique <henrique.dias@gruponewway.com.br>
2023-04-02 11:23:33 -0300

    refactor: adjusting searchCards

--- grpc/server/services/yugioh/yugioh-service-implementation.js
+++ grpc/server/services/yugioh/yugioh-service-implementation.js
@@ -1,26 +1,22 @@
 const { default: axios } = require("axios");
 
-function searchCards(call,callback) {
-  let searchQuery = call.request.searchQuery;
+async function searchCards(call,callback) {
+  let { searchQuery } = call.request;
   searchQuery = searchQuery.trim().toLowerCase();
-  const apiUrl = `${process.env.API_BASE_URL || 'https://db.ygoprodeck.com/api/v7/cardinfo.php'}?fname=${searchQuery}`;
+  const apiUrl = `${process.env.API_BASE_URL ?? 'https://db.ygoprodeck.com/api/v7/cardinfo.php'}?fname=${searchQuery}`;
 
-  axios.get(apiUrl)
-    .then((response) => {
-      const cards = response.data.data;
-      callback(null,{
-        cards: cards
-      });
-    })
-    .catch((error) => {
-      error.message = error?.response?.data?.error || error?.message || 'Server error';
-      if (error?.message?.match(/^No card matching your query was found in the database/)) {
-        callback(null,{ cards: [] })
-        return
-      }
-      console.log(`[grpc/server/services/yugioh-service-implementation.js searchCards] Error: ${error.message}`);
-      callback(error);
-    });
+  try {
+    const { data: { data: cards } } = await axios.get(apiUrl);
+    callback(null,{ cards });
+  } catch (error) {
+    error.message = error?.response?.data?.error ?? error?.message ?? 'Server error';
+    if (error?.message?.match(/^No card matching your query was found in the database/)) {
+      callback(null,{ cards: [] });
+      return;
+    }
+    console.log(`[grpc/server/services/yugioh-service-implementation.js searchCards] Error: ${error.message}`);
+    callback(error);
+  }
 }
 
 
